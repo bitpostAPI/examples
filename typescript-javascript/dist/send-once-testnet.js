38cdbb940316e0a0c6027340d575cbbfe9506873
pedro-roberto <pedro.fr.22@gmail.com>
2020-10-14 04:15:46 +0100

    Add typescript/javascript quick start code.

---
+++ typescript-javascript/dist/send-once-testnet.js
@@ -0,0 +1,110 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var bitcoin = require("bitcoinjs-lib");
+var coinSelect = require("coinselect");
+var bip32 = require("bip32");
+var bip39 = require("bip39");
+var request = require("sync-request");
+var bitpost_1 = require("bitpost");
+// input ---------------------------------------------
+var maxDollarFee = 3;
+var destinationAddress = 'mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt';
+var satsToSend = 566;
+var confirmation_target_seconds = Math.round(Date.now() / 1000) + 60 * 60; // eg. in one hour
+var mnemonicSeed = 'your bip39 word list';
+// ----------------------------------------------------
+var seed = bip39.mnemonicToSeedSync(mnemonicSeed);
+var master = bip32.fromSeed(seed, bitcoin.networks.testnet);
+var mainKey = master.derivePath("m/84'/0'/0'/0/0");
+var changeKey = master.derivePath("m/84'/0'/0'/1/0");
+var mainAddress = bitcoin.payments.p2wpkh({ pubkey: mainKey.publicKey, network: bitcoin.networks.testnet });
+var changeAddress = bitcoin.payments.p2wpkh({ pubkey: changeKey.publicKey, network: bitcoin.networks.testnet });
+var bitpostInterface = new bitpost_1.BitpostInterfaceForBitcoinJS({ testnet: true });
+function getUtxosFromAddress(address) {
+    var resp = request('GET', 'https://blockstream.info/testnet/api/address/' + address + '/utxo');
+    var utxosRaw = JSON.parse(resp.getBody('utf-8'));
+    return utxosRaw.map(function (utxo) {
+        utxos.push({
+            txId: utxo.txid,
+            vout: utxo.vout,
+            value: utxo.value,
+            witnessUtxo: {
+                script: bitcoin.address.toOutputScript(address, bitcoin.networks.testnet),
+                value: utxo.value
+            }
+        });
+    });
+}
+var utxos = [];
+utxos.concat(getUtxosFromAddress(mainAddress.address));
+utxos.concat(getUtxosFromAddress(changeAddress.address));
+var BTC_PRICE_USD = 10000; //can be easily fetched through an API
+var MAX_FEE_IN_SATS = 100000000 * maxDollarFee / BTC_PRICE_USD;
+// you should use the exact transaction size, this is not adviced for production
+var HEURISTIC_TX_SIZE = 10 + 34 * 2 + 68; // 2 outputs and one segwit input
+var USER_MAX_FEERATE = MAX_FEE_IN_SATS / HEURISTIC_TX_SIZE;
+var balance = utxos.map(function (i) { return i.value; }).reduce(function (x, y) { return x + y; });
+if (balance < satsToSend + MAX_FEE_IN_SATS)
+    throw "Insufficient balance.";
+var payees = [{ address: destinationAddress, value: satsToSend }];
+var _a = coinSelect(utxos, payees, Math.round(USER_MAX_FEERATE * 2 / 3)), inputs = _a.inputs, outputs = _a.outputs, _ = _a._;
+function make_transaction(inputs, outputs, targetFeerate, txSize) {
+    if (Boolean(targetFeerate) !== Boolean(txSize))
+        throw "TargetFeerate and txSize arguments must be both provided or left in blank.";
+    var tx = new bitcoin.Psbt({ network: bitcoin.networks.testnet });
+    if (targetFeerate) {
+        tx.setMaximumFeeRate(targetFeerate + 1);
+    }
+    var initialFee = 0;
+    inputs.forEach(function (input) {
+        initialFee += input.value;
+        tx.addInput({
+            hash: input.txId,
+            index: input.vout,
+            sequence: 0xfffffffd,
+            witnessUtxo: {
+                script: input.witnessUtxo.script,
+                value: input.witnessUtxo.value,
+            },
+        });
+    });
+    outputs.forEach(function (o) { return initialFee -= o.value; });
+    outputs.forEach(function (output) {
+        if (!output.address && targetFeerate) {
+            var adjustedChange = output.value + initialFee - txSize * targetFeerate;
+            tx.addOutput({ address: changeAddress.address, value: adjustedChange });
+        }
+        else if (!output.address) {
+            tx.addOutput({ address: changeAddress.address, value: output.value });
+        }
+        else {
+            tx.addOutput({ address: output.address, value: output.value });
+        }
+    });
+    for (var i = 0; i < inputs.length; i++) {
+        try {
+            tx.signInput(i, mainKey);
+        }
+        catch (_a) { }
+        try {
+            tx.signInput(i, changeKey);
+        }
+        catch (_b) { }
+    }
+    tx.validateSignaturesOfAllInputs();
+    tx.finalizeAllInputs();
+    return tx;
+}
+var preliminary_tx = make_transaction(inputs, outputs);
+var txSize = preliminary_tx.extractTransaction().virtualSize();
+var feerates = bitpostInterface.getFeerates({ maxFeerate: USER_MAX_FEERATE });
+var rawTxs = [];
+for (var _i = 0, feerates_1 = feerates; _i < feerates_1.length; _i++) {
+    var feerate = feerates_1[_i];
+    var final_tx = make_transaction(inputs, outputs, feerate, txSize);
+    rawTxs.push(final_tx.extractTransaction().toHex());
+}
+var delay = 0;
+var bitpostRequest = bitpostInterface.createBitpostRequest(rawTxs, confirmation_target_seconds, delay);
+var response = bitpostRequest.sendRequest();
+//# sourceMappingURL=send-once-testnet.js.map
---
+++ typescript-javascript/send-once-testnet.ts
@@ -0,0 +1,121 @@
+import bitcoin = require("bitcoinjs-lib");
+import coinSelect = require("coinselect");
+import bip32 = require("bip32");
+import bip39 = require("bip39");
+const request = require("sync-request");
+import {BitpostInterfaceForBitcoinJS, BitpostRequest} from "bitpost"
+
+// input ---------------------------------------------
+let maxDollarFee: number = 3
+const destinationAddress: string = 'mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt';
+const satsToSend: number = 566;
+const confirmation_target_seconds = Math.round(Date.now()/1000) + 60 * 60  // eg. in one hour
+let mnemonicSeed: string = 'your bip39 word list'
+// ----------------------------------------------------
+
+let seed = bip39.mnemonicToSeedSync(mnemonicSeed)
+let master: bip32.BIP32Interface = bip32.fromSeed(seed, bitcoin.networks.testnet);
+let mainKey: bip32.BIP32Interface = master.derivePath(`m/84'/0'/0'/0/0`); 
+let changeKey: bip32.BIP32Interface = master.derivePath(`m/84'/0'/0'/1/0`);
+let mainAddress = bitcoin.payments.p2wpkh({ pubkey: mainKey.publicKey, network: bitcoin.networks.testnet});
+let changeAddress = bitcoin.payments.p2wpkh({ pubkey: changeKey.publicKey, network: bitcoin.networks.testnet });
+
+let bitpostInterface: BitpostInterfaceForBitcoinJS = new BitpostInterfaceForBitcoinJS({testnet:true})
+
+interface TxInput{txId: string, vout: number, value: number, witnessUtxo: WitnessUtxo}
+function getUtxosFromAddress(address: string): Array<TxInput>{
+  var resp = request('GET', 'https://blockstream.info/testnet/api/address/' + address + '/utxo');
+  let utxosRaw = JSON.parse(resp.getBody('utf-8'));
+  return utxosRaw.map(utxo => {utxos.push({
+    txId: utxo.txid,
+    vout: utxo.vout,
+    value: utxo.value,
+    witnessUtxo: {
+      script: bitcoin.address.toOutputScript(address, bitcoin.networks.testnet),
+      value: utxo.value
+    }
+  })})
+}
+let utxos: Array<TxInput> = []
+utxos.concat(getUtxosFromAddress(mainAddress.address))
+utxos.concat(getUtxosFromAddress(changeAddress.address))
+
+const BTC_PRICE_USD: number = 10_000 //can be easily fetched through an API
+const MAX_FEE_IN_SATS: number = 100_000_000*maxDollarFee/BTC_PRICE_USD
+// you should use the exact transaction size, this is not adviced for production
+const HEURISTIC_TX_SIZE = 10 + 34*2 + 68 // 2 outputs and one segwit input
+const USER_MAX_FEERATE = MAX_FEE_IN_SATS/HEURISTIC_TX_SIZE
+
+const balance: number = utxos.map(i => i.value).reduce((x,y)=>x+y)
+if(balance < satsToSend+MAX_FEE_IN_SATS) throw "Insufficient balance."
+
+let payees = [{address: destinationAddress, value: satsToSend}]
+let { inputs, outputs, _ } = coinSelect(utxos, payees, Math.round(USER_MAX_FEERATE*2/3))
+
+
+interface TxOutput{address: string, value: number, network: bitcoin.Network}
+interface WitnessUtxo{script: Buffer, value: number}
+
+function make_transaction(inputs: Array<TxInput>, outputs: Array<TxOutput>, targetFeerate?: number, txSize?: number): bitcoin.Psbt {
+  if(Boolean(targetFeerate) !== Boolean(txSize)) throw "TargetFeerate and txSize arguments must be both provided or left in blank."
+  
+  let tx: bitcoin.Psbt = new bitcoin.Psbt({network: bitcoin.networks.testnet});
+  if(targetFeerate){
+    tx.setMaximumFeeRate(targetFeerate + 1);
+  }
+  let initialFee = 0;
+
+  inputs.forEach(input => {
+    initialFee += input.value
+    tx.addInput({
+      hash: input.txId,
+      index: input.vout,
+      sequence: 0xfffffffd,
+      witnessUtxo: {
+        script: input.witnessUtxo.script,
+        value: input.witnessUtxo.value,
+      },
+    });
+  });
+  
+  outputs.forEach(o => initialFee -= o.value)
+
+  outputs.forEach(output => {
+    if(!output.address && targetFeerate){
+      const adjustedChange: number = output.value + initialFee - txSize*targetFeerate 
+      tx.addOutput({address: changeAddress.address, value: adjustedChange})
+    } else if (!output.address) {
+      tx.addOutput({address: changeAddress.address, value: output.value})
+    } else {
+      tx.addOutput({address: output.address, value: output.value})
+    }
+  })
+  
+  for(let i=0; i < inputs.length; i++){
+    try{
+      tx.signInput(i, mainKey);
+    }catch{}
+    try{
+      tx.signInput(i, changeKey);
+    }catch{}
+  }
+  
+  tx.validateSignaturesOfAllInputs();
+  tx.finalizeAllInputs();
+  return tx;
+}
+
+let preliminary_tx: bitcoin.Psbt = make_transaction(inputs, outputs);
+let txSize: number = preliminary_tx.extractTransaction().virtualSize();
+
+let feerates: Array<number> = bitpostInterface.getFeerates({maxFeerate: USER_MAX_FEERATE})
+let rawTxs: Array<string> = []
+for(let feerate of feerates){
+  let final_tx = make_transaction(inputs, outputs, feerate, txSize);
+  rawTxs.push(final_tx.extractTransaction().toHex())
+}
+
+const delay = 0
+let bitpostRequest: BitpostRequest = bitpostInterface.createBitpostRequest(
+                                      rawTxs, confirmation_target_seconds, delay)
+let response = bitpostRequest.sendRequest()
